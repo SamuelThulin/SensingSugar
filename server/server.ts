2a49c57d8ff95b4ee4f8b83471d6e297c6ea1e8e
Luciano Frizzera <lucaju@me.com>
2022-02-20 21:59:07 -0500

    feat: 🎸 server: catch and show error 404

--- server/server.ts
+++ server/server.ts
@@ -17,6 +17,15 @@ if (process.env.NODE_ENV === 'development') loadDevTools();
 
 app.use(express.static(publicPath));
 
+// catch all
+// * turno off on dev. reason HMR doesn't work with this on.
+// if (process.env.NODE_ENV !== 'development') {
+app.get('*', (req, res) => {
+  // console.log('catch all');
+  // res.set('Content-Type', 'text/event-stream');
+  res.status(200).sendFile(path.join(publicPath, 'index.html'));
+});
+
 // app.use('*', express.static(path.join(publicPath, 'index.html')));
 
 export default app;
